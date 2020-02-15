e1ebba800a7b1954249ca03bbbb0529debcf5435
Raynos <raynos2@gmail.com>
2020-02-15 14:46:52 +0100

    move methods inline into classes

--- src/index.js
+++ src/index.js
@@ -1,4 +1,9 @@
+'use strict'
+
 const assert = require('assert')
+
+const { Table } = require('./table')
+const { getDynamoDataType } = require('./util')
 
 class Database {
   constructor (AWS, opts = {}) {
@@ -10,11 +15,116 @@ class Database {
     this.opts = opts
     this.tables = {}
   }
+
+  async open (TableName, opts = {}) {
+    assert(TableName, 'a table name parameter is required')
+
+    if (this.tables[TableName]) {
+      return { table: this.tables[TableName] } // the table is already "open".
+    }
+
+    const table = this.tables[TableName] = new Table(this.AWS, this.opts, opts)
+
+    try {
+      const { Table } = await this.db.describeTable({ TableName }).promise()
+      table.meta = Table
+    } catch (err) {
+      return { err }
+    }
+
+    const schema = table.meta.KeySchema
+    const defs = table.meta.AttributeDefinitions
+
+    const hash = schema.find(o => o.KeyType === 'HASH')
+    const range = schema.find(o => o.KeyType === 'RANGE')
+
+    const keyDef = defs.find(o => o.AttributeName === hash.AttributeName)
+    table.hashKey = hash.AttributeName
+    table.hashType = keyDef.AttributeType
+
+    if (range) {
+      const rangeDef = defs.find(o => o.AttributeName === range.AttributeName)
+      table.rangeKey = range.AttributeName
+      table.rangeType = rangeDef.AttributeType
+    }
+
+    return { table }
+  }
+
+  async create (
+    TableName, hash = 'hash',
+    range = 'range',
+    opts = {}
+  ) {
+    assert(TableName, 'a table name parameter is required')
+
+    const params = {
+      TableName,
+      AttributeDefinitions: [{
+        AttributeName: hash,
+        AttributeType: getDynamoDataType(hash)
+      }],
+      KeySchema: [{
+        AttributeName: hash,
+        KeyType: 'HASH'
+      }]
+    }
+
+    if (range) {
+      params.AttributeDefinitions.push({
+        AttributeName: range,
+        AttributeType: getDynamoDataType(range)
+      })
+
+      params.KeySchema.push({
+        AttributeName: range,
+        KeyType: 'RANGE'
+      })
+    }
+
+    params.BillingMode = 'PAY_PER_REQUEST'
+
+    params.SSESpecification = {
+      Enabled: true
+    }
+
+    Object.assign(params, opts)
+
+    try {
+      await this.db.createTable(params).promise()
+    } catch (err) {
+      return { err }
+    }
+
+    try {
+      await this.db.waitFor('tableExists', { TableName }).promise()
+    } catch (err) {
+      return { err }
+    }
+
+    while (true) {
+      let data = {}
+
+      try {
+        data = await this.db.describeTable({ TableName }).promise()
+      } catch (err) {
+        return { err }
+      }
+
+      console.log('create', data.Table)
+      if (data.Table.TableStatus !== 'ACTIVE') {
+        await sleep(2e3)
+      } else {
+        break
+      }
+    }
+
+    return { table: this }
+  }
 }
 
-Object.assign(Database.prototype, {
-  ...require('./create'),
-  ...require('./open')
-})
-
 module.exports = Database
+
+async function sleep (n) {
+  return new Promise(resolve => setTimeout(resolve, n))
+}
--- src/table.js
+++ src/table.js
@@ -1,3 +1,5 @@
+const { queryParser, toJSON, toDynamoJSON } = require('./util')
+
 class Table {
   constructor (AWS, dbOpts = {}, opts = {}) {
     this.disableATD = opts.disableATD
@@ -15,18 +17,377 @@ class Table {
 
     return o
   }
-}
 
-Object.assign(Table.prototype, {
-  ...require('./ttl'),
-  ...require('./batch'),
-  ...require('./update'),
-  ...require('./count'),
-  ...require('./delete'),
-  ...require('./get'),
-  ...require('./put'),
-  ...require('./query'),
-  ...require('./util')
-})
+  async setTTL (AttributeName = 'ttl', Enabled = true) {
+    if (!this.meta && this.meta.TableName) {
+      return { err: new Error('No table open') }
+    }
+
+    let data = {}
+    let isEnabled = false
+    const TableName = this.meta.TableName
+
+    try {
+      data = await this.db.describeTimeToLive({ TableName }).promise()
+
+      isEnabled = (
+        data.TimeToLiveDescription &&
+        data.TimeToLiveDescription.TimeToLiveStatus &&
+        data.TimeToLiveDescription.TimeToLiveStatus === 'ENABLED'
+      )
+    } catch (err) {
+      return { err }
+    }
+
+    if (isEnabled && Enabled) {
+      return { table: this }
+    }
+
+    const params = {
+      TableName,
+      TimeToLiveSpecification: {
+        AttributeName,
+        Enabled
+      }
+    }
+
+    try {
+      await this.db.updateTimeToLive(params).promise()
+    } catch (err) {
+      return { err }
+    }
+
+    return { table: this }
+  }
+
+  async batchWrite (batch, opts = {}) {
+    const ops = batch.map(({ 0: hash, 1: range, 2: value }) => {
+      const Key = this.createKeyProperties(hash, range)
+
+      if (value) {
+        return {
+          PutRequest: {
+            Item: {
+              ...Key,
+              ...toDynamoJSON(value)
+            }
+          }
+        }
+      }
+
+      return {
+        DeleteRequest: { Key }
+      }
+    })
+
+    {
+      const params = {
+        RequestItems: {
+          [this.meta.TableName]: ops,
+          ...opts
+        }
+      }
+
+      try {
+        await this.db.batchWriteItem(params).promise()
+      } catch (err) {
+        return { err }
+      }
+    }
+
+    return {}
+  }
+
+  async batchRead (batch, opts = {}) {
+    const { TableName } = this.meta
+
+    const params = {
+      RequestItems: {
+        [TableName]: {
+          Keys: batch.map(({ 0: hash, 1: range }) => {
+            return this.createKeyProperties(hash, range)
+          })
+        }
+      },
+      ...opts
+    }
+
+    let data = {}
+
+    try {
+      data = await this.db.batchGetItem(params).promise()
+    } catch (err) {
+      return { err }
+    }
+
+    return { data: data.Responses[TableName] }
+  }
+
+  async update (hash, range, dsl, opts = {}) {
+    if (typeof dsl === 'object') {
+      dsl = range
+      opts = {}
+    }
+
+    const {
+      attributeValues: ExpressionAttributeValues,
+      attributeNames: ExpressionAttributeNames,
+      expression: UpdateExpression
+    } = queryParser(dsl)
+
+    const params = {
+      Key: this.createKeyProperties(hash, range),
+      TableName: this.meta.TableName,
+      ExpressionAttributeNames,
+      ExpressionAttributeValues,
+      UpdateExpression,
+      ReturnValues: 'ALL_NEW',
+      ...opts
+    }
+
+    try {
+      await this.db.updateItem(params).promise()
+    } catch (err) {
+      return { err }
+    }
+
+    return {}
+  }
+
+  async count (isManualCount, opts) {
+    const params = {
+      TableName: this.meta.TableName,
+      ...opts
+    }
+
+    if (!isManualCount) {
+      let data = {}
+
+      try {
+        data = await this.db.describeTable(params).promise()
+      } catch (err) {
+        return { err }
+      }
+
+      return { data: data.Table.ItemCount }
+    }
+
+    params.Select = 'COUNT'
+
+    let count = 0
+
+    while (true) {
+      let data = null
+
+      try {
+        data = await this.db.scan(params).promise()
+      } catch (err) {
+        return { err }
+      }
+
+      count += data.Count
+
+      if (!data.LastEvaluatedKey) {
+        break
+      }
+
+      params.ExclusiveStartKey = data.LastEvaluatedKey
+    }
+
+    return { data: count, LastEvaluatedKey: params.LastEvaluatedKey }
+  }
+
+  async delete (hash, range, props = {}) {
+    if (typeof range !== 'string') {
+      props = range || {}
+    }
+
+    const params = {
+      Key: this.createKeyProperties(hash, range),
+      TableName: this.meta.TableName
+    }
+
+    try {
+      await this.db.deleteItem(params).promise()
+    } catch (err) {
+      return { err }
+    }
+
+    return {}
+  }
+
+  async get (hash, range, opts = {}) {
+    if (typeof range !== 'string') {
+      opts = range || {}
+    }
+
+    const params = {
+      Key: this.createKeyProperties(hash, range),
+      TableName: this.meta.TableName,
+      ...opts
+    }
+
+    let data = null
+
+    try {
+      data = await this.db.getItem(params).promise()
+    } catch (err) {
+      return { err }
+    }
+
+    if (!data.Item) {
+      return { err: { notFound: true } }
+    }
+
+    return { data: toJSON(data.Item) }
+  }
+
+  async put (hash, range, props = {}, opts = {}) {
+    if (typeof range !== 'string') {
+      opts = props || {}
+      props = range || {}
+    }
+
+    const params = {
+      Item: {
+        ...this.createKeyProperties(hash, range),
+        ...this.disableATD ? props : toDynamoJSON(props)
+      },
+      TableName: this.meta.TableName,
+      ...opts
+    }
+
+    try {
+      await this.db.putItem(params).promise()
+    } catch (err) {
+      if (err.message === 'The conditional request failed') {
+        return { err: { exists: true } }
+      }
+
+      return { err }
+    }
+
+    return {}
+  }
+
+  async putNew (hash, range, props = {}, opts = {}) {
+    const condition = ['attribute_not_exists(#hash)']
+
+    opts.ExpressionAttributeNames = {
+      '#hash': this.hashKey
+    }
+
+    if (this.rangeKey) {
+      condition.push('AND', 'attribute_not_exists(#range)')
+      opts.ExpressionAttributeNames['#range'] = this.rangeKey
+    }
+
+    opts.ConditionExpression = condition.join(' ')
+    return this.put(hash, range, props, opts)
+  }
+
+  query (dsl, opts = {}) {
+    return this.iterator(dsl, opts, 'query')
+  }
+
+  scan (dsl, opts = {}) {
+    return this.iterator(dsl, opts, 'scan')
+  }
+
+  iterator (dsl, opts = {}, method) {
+    const {
+      expression,
+      attributeValues: ExpressionAttributeValues,
+      attributeNames: ExpressionAttributeNames
+    } = queryParser(dsl)
+
+    if (!Object.keys(ExpressionAttributeValues).length) {
+      throw new Error('Query has no values')
+    }
+
+    if (!expression.length) {
+      throw new Error('Query is empty')
+    }
+
+    const params = {
+      TableName: this.meta.TableName,
+      ExpressionAttributeValues,
+      ExpressionAttributeNames,
+      ...opts
+    }
+
+    if (method === 'query') {
+      params.KeyConditionExpression = expression
+    } else {
+      params.FilterExpression = expression
+    }
+
+    let values = []
+    let iteratorIndex = 0
+    let isFinished = false
+
+    const hashKey = this.hashKey
+    const hashType = this.hashType
+    const rangeKey = this.rangeKey
+    const rangeType = this.rangeType
+
+    return {
+      [Symbol.asyncIterator] () {
+        return this
+      },
+      filter: async dsl => {
+        const {
+          attributeValues: ExpressionAttributeValues,
+          expression: FilterExpression
+        } = queryParser(dsl)
+
+        params.FilterExpression = FilterExpression
+        params.ExpressionAttributeValues = Object.assign(
+          params.ExpressionAttributeValues,
+          ExpressionAttributeValues
+        )
+      },
+      properties: async dsl => {
+        params.ProjectionExpression = dsl
+      },
+      next: async () => {
+        if (iteratorIndex < values.length) {
+          return { value: values[iteratorIndex++] }
+        }
+
+        if (isFinished) {
+          return { done: true }
+        }
+
+        const res = await this.db[method](params).promise()
+
+        if (res.Items) {
+          const restructured = res.Items.map(item => {
+            const key = [item[hashKey][hashType]]
+            delete item[hashKey]
+
+            if (rangeKey) {
+              const range = item[rangeKey][rangeType]
+              delete item[this.rangeKey]
+              if (range) key.push(range)
+            }
+
+            return { key, value: toJSON(item) }
+          })
+
+          values = [...values, ...restructured]
+        }
+
+        if (typeof res.LastEvaluatedKey === 'undefined' || params.Limit) {
+          isFinished = true
+        } else {
+          params.ExclusiveStartKey = res.LastEvaluatedKey
+        }
+
+        return { value: values[iteratorIndex++] }
+      }
+    }
+  }
+}
 
 module.exports = { Table }
